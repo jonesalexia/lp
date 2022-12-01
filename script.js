6dfc69b93822f08e88d3a8749de7c86e94d9e509
jonesalexia <alexiasojones@gmail.com>
2022-12-01 20:26:31 -0300

    missing mobile settings

---
+++ script.js
@@ -0,0 +1,33 @@
+// button hover
+
+document.getElementsByClassName("column")[0].onmouseover = function () {
+    document.getElementsByClassName("card-button")[0].style.background = "#2D9CDB";
+    document.getElementsByClassName("card-button")[0].style.color = "#FFFFFF";
+}
+
+document.getElementsByClassName("column")[0].onmouseout = function () {
+    document.getElementsByClassName("card-button")[0].style.background = "#E0E0E0";
+    document.getElementsByClassName("card-button")[0].style.color = "#828282";
+}
+
+document.getElementsByClassName("column")[1].onmouseover = function () {
+    document.getElementsByClassName("card-button")[1].style.background = "#2D9CDB";
+    document.getElementsByClassName("card-button")[1].style.color = "#FFFFFF";
+}
+
+document.getElementsByClassName("column")[1].onmouseout = function () {
+    document.getElementsByClassName("card-button")[1].style.background = "#E0E0E0";
+    document.getElementsByClassName("card-button")[1].style.color = "#828282";
+} 
+
+document.getElementsByClassName("column")[2].onmouseover = function () {
+    document.getElementsByClassName("card-button")[2].style.background = "#2D9CDB";
+    document.getElementsByClassName("card-button")[2].style.color = "#FFFFFF";
+}
+
+document.getElementsByClassName("column")[1].onmouseout = function () {
+    document.getElementsByClassName("card-button")[2].style.background = "#E0E0E0";
+    document.getElementsByClassName("card-button")[2].style.color = "#828282";
+} 
+
+  
